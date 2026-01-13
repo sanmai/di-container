c3f1a48d4d440faebe815c4fbe84ae876639ca22
Alexey Kopytko <alexey@kopytko.com>
2026-01-13 16:17:55 +0900

    Apply suggestion from @sanmai

--- tests/ContainerTest.php
+++ tests/ContainerTest.php
@@ -319,7 +319,7 @@ class ContainerTest extends TestCase
         $this->assertInstanceOf(SimpleObject::class, $container->get(SimpleObject::class));
     }
 
-    public function testFactoryWithContainerOnSelfType(): void
+    public function testFactorySelfType(): void
     {
         $container = new ExtendedContainer();
         $result = $container->withSelfFactory();
