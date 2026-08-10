a674e6d43de5d5713ca5050152f6474280b26bef
Alexey Kopytko <alexey@kopytko.com>
2026-08-10 11:46:16 +0900

    fix

--- src/Container.php
+++ src/Container.php
@@ -365,7 +365,7 @@ class Container implements ContainerInterface, IteratorAggregate
     private function providersForType(string $type): array
     {
         /** @var list<class-string<object>> */
-        return take($this->factories, $this->builders, $this->implementations, $this->prebuilt)
+        return take($this)
             ->keys()
             ->filter(static fn(string $id) => is_a($id, $type, true))
             ->toList();
